7f9fa517c5b829249a8692ab43ea4a75ca87c1bb
Abhishek Kumar Gupta <117657114+Abhishekk1205@users.noreply.github.com>
2022-12-02 03:02:53 +0530

    Area of circle
    Python Programme to find area of circle

---
+++ abc.py
@@ -0,0 +1,4 @@
+import math as M  
+Radius = float (input ("Input the radius of circle: "))  
+area_of_the_circle = M.pi* Radius * Radius  
+print ("The area of the circle with radius ",Radius ," is ",area_of_the_circle)  
